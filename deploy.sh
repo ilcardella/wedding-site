d4b4276631f47e18a3d8078264465ebdb08cfb5e
Alberto Cardellini <albe.carde@gmail.com>
2019-10-13 20:31:11 +0100

    Deploy restart server (#2)
    * moved server for development in docker compose
    
    * Added deploy script and encrypted environment variables
    
    * Removed travis addon
    
    * Encrypted env variables
    
    * Added deployment for staging builds
    
    * Fixed travis condition
    
    * Corrected deploy script
    
    * Corrected deploy script
    
    * Corrected deploy script
    
    * Corrected deploy script
    
    * Corrected deploy script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Bash safety settings
+set -eu -o pipefail
+
+# For staging builds deploy in /tmp
+if [[ $1 == "staging" ]]; then
+    DEPLOY_DIRECTORY="/tmp/"
+fi
+
+DEPLOY_SSH_PATH="${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_DIRECTORY}"
+
+# Deploy the content of the public folder
+rsync -r --quiet --delete-after ${TRAVIS_BUILD_DIR}/public/* ${DEPLOY_SSH_PATH}/public/
+# Deploy the docker-compose file
+rsync --quiet ${TRAVIS_BUILD_DIR}/docker-compose.yml ${DEPLOY_SSH_PATH}
+# Deploy the .env file created during the CI build
+rsync --quiet ${TRAVIS_BUILD_DIR}/.env ${DEPLOY_SSH_PATH}
+
+if [[ $1 == "production" ]]; then
+# Restart the server container
+ssh ${DEPLOY_USER}@${DEPLOY_HOST} /bin/bash <<EOF
+cd ${DEPLOY_DIRECTORY}
+docker-compose down
+EOF
+
+ssh ${DEPLOY_USER}@${DEPLOY_HOST} /bin/bash <<EOF
+cd ${DEPLOY_DIRECTORY}
+docker-compose up -d nginx-server
+EOF
+fi
